98ea3bcec3f64b01c99eb15d726481f2706bfd89
mamathanaikk22 <mamathanaik.k22@gmail.com>
2026-03-06 16:21:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,7 +38,7 @@ function App() {
   return (
     <div style={{textAlign:"center", padding:"40px"}}>
 
-      <h1>Music FInder</h1>
+      <h1>Mood reseter</h1>
 
       <input
         type="text"
